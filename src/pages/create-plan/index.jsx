937db2bd40a0b85ee63f73df4253e247fb52df9f
aqsafaisal34 <aqsay.faisal34@gmail.com>
2023-11-16 12:09:26 -0800

    add few changes

--- src/pages/create-plan/index.jsx
+++ src/pages/create-plan/index.jsx
@@ -1,14 +1,20 @@
-import "./style.css"
-import { Button, Grid } from "@cloudscape-design/components"
-import PlanSetting from "./PlanSetting"
-import PlanCalendar from "./PlanCalendar"
-import PlanMetircs from "./PlanMetircs"
-import Platform from "./Platform"
-import DayParts from "./DayParts"
+import "./style.css";
+import { Button, Grid } from "@cloudscape-design/components";
+import PlanSetting from "./PlanSetting";
+import PlanCalendar from "./PlanCalendar";
+import PlanMetircs from "./PlanMetircs";
+import Platform from "./Platform";
+import DayParts from "./DayParts";
 import AudienceSelection from "./AudienceSelection";
 import Header from '../../components/header';
 import { useNavigate } from "react-router-dom";
 import './style.css';
+import toast, { Toaster } from 'react-hot-toast';
+
+const notify = () => {
+     toast.success('Plan Created Successfully');
+     console.log("Plan Created Successfully")
+}
 
 
 const CreatePlan = () => {
@@ -17,6 +23,7 @@ const CreatePlan = () => {
         e.preventDefault()
         navigate('/dashboard')
       }
+
     return ( 
         <>
         <Header title={"Create Plan"} button={"Dashboard"} function={submitHandlerTwo}/>
@@ -32,7 +39,11 @@ const CreatePlan = () => {
                 <h2 className="font-weight-bold">Summary</h2>
                 <PlanCalendar/>
                 <PlanMetircs/>
-                <Button variant="" className="mt-4 float-right custom-button">Create Plan</Button>
+                <Button variant=" primary" className="mt-4 float-right custom-button" onClick={notify}>Create Plan</Button>
+                <Toaster
+                position="bottom-center"
+                reverseOrder={false}
+                 />
             </div>
         </Grid>
         </>
